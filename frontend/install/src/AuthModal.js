403e0d54f0ee2657e49299be40a6ad7eb544b0af
Aman Adhav <amanadhav@gmail.com>
2021-12-31 13:12:29 -0500

    added git ignore small change

--- frontend/install/src/AuthModal.js
+++ frontend/install/src/AuthModal.js
@@ -8,7 +8,7 @@ function AuthModal() {
             <div className='border border-crypdit_dark-search_text w-1/2 sm:w-1/4 bg-crypdit_dark p-5 text-crypdit_text mx-4 self-center mx-auto rounded-md'>
                 <h1 className="text-2xl mb-3 text-center font-bold">Login with Metamask</h1>
                 <h1 className="text-center m-4">Please connect to the Rinkeby Test Network</h1>
-                <button className="w-full justify-center border border-gray-300 rounded-full flex px-3 hover:bg-gray-300 hover:text-black items-stretch text-sm font-bold h-8" style={{borderRadius:'.3rem'}}>
+                <button className="w-full justify-center border border-gray-300 rounded-full flex px-3 hover:bg-gray-300 hover:text-black items-stretch text-sm font-bold h-8" style={{borderRadius:'.3rem'}} onClick={connectWallet()}>
                     <img src={MetamaskLogo} alt="" className="w-8 h-8"/>
                     <div className="m-1">Log In</div>
                 </button>
