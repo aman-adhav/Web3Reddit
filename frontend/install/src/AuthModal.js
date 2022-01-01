31a367b9c529bed7f3700e2f16ba775959d4f714
Aman Adhav <amanadhav@gmail.com>
2021-12-31 23:03:31 -0500

    finished authetication modal

--- frontend/install/src/AuthModal.js
+++ frontend/install/src/AuthModal.js
@@ -1,22 +1,55 @@
 import MetamaskLogo from './metamask-fox.svg';
-import {useState} from 'react';
+import {useContext} from 'react';
+import AuthModalContext from './AuthModalContext';
+import ClickOutHandler from 'react-clickout-handler';
+import AccountContext from './AccountContext';
 
 function AuthModal() {
 
+    const modalContext = useContext(AuthModalContext)
+    const visibleClass = modalContext.show ? 'block' : 'hidden'; 
+
+    const account = useContext(AccountContext);
+
+
+    const connectWallet = async () => {
+        try {
+          const { ethereum } = window;
+    
+          if (!ethereum) {
+            alert("Get MetaMask!");
+            return;
+          }
+    
+          const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+    
+          console.log("Connected", accounts[0]);
+
+          account.setPublicKey(accounts[0]);
+
+          modalContext.setShow(false);
+          //setCurrentAccount(accounts[0]);
+        } catch (error) {
+          console.log(error)
+        }
+    }
+    
+
     return (
-        <div className='w-screen h-screen absolute top-0 left-0 z-20 flex' style={{backgroundColor:'rgba(0,0,0,.6)'}}>
-            <div className='border border-crypdit_dark-search_text w-1/2 sm:w-1/4 bg-crypdit_dark p-5 text-crypdit_text mx-4 self-center mx-auto rounded-md'>
-                <h1 className="text-2xl mb-3 text-center font-bold">Login with Metamask</h1>
-                <h1 className="text-center m-4">Please connect to the Rinkeby Test Network</h1>
-                <button className="w-full justify-center border border-gray-300 rounded-full flex px-3 hover:bg-gray-300 hover:text-black items-stretch text-sm font-bold h-8" style={{borderRadius:'.3rem'}} onClick={connectWallet()}>
-                    <img src={MetamaskLogo} alt="" className="w-8 h-8"/>
-                    <div className="m-1">Log In</div>
-                </button>
-                <h5 className='text-center m-4'>To get test ETH use a <a href="https://faucets.chain.link/rinkeby" className='text-green-700'>verified faucet</a>.</h5>
-            </div>
+        <div className={'w-screen h-screen absolute top-0 left-0 z-20 flex ' + visibleClass} style={{backgroundColor:'rgba(0,0,0,.6)'}}>
+            <ClickOutHandler onClickOut={() => modalContext.setShow(false)}>
+                <div className='border border-crypdit_dark-search_text w-1/2 sm:w-1/4 bg-crypdit_dark p-5 text-crypdit_text mx-4 self-center mx-auto rounded-md'>
+                    <h1 className="text-2xl mb-3 text-center font-bold">Login with Metamask</h1>
+                    <h1 className="text-center m-4">Please connect to the Rinkeby Test Network</h1>
+                    <button className="w-full justify-center border border-gray-300 rounded-full flex px-3 hover:bg-gray-300 hover:text-black items-stretch text-sm font-bold h-8" style={{borderRadius:'.3rem'}} onClick={connectWallet}>
+                        <img src={MetamaskLogo} alt="" className="w-8 h-8"/>
+                        <div className="m-1">Log In</div>
+                    </button>
+                    <h5 className='text-center m-4'>To get test ETH use a <a href="https://faucets.chain.link/rinkeby" className='text-green-700'>verified faucet</a>.</h5>
+                </div>
+            </ClickOutHandler>
         </div>
     );
-
 }
 
 export default AuthModal;
--- frontend/install/src/Header.js
+++ frontend/install/src/Header.js
@@ -1,9 +1,11 @@
 import Logo from './weddit-logo.png';
-import {SearchIcon, ChevronDownIcon, UserIcon, LoginIcon, PlusIcon} from '@heroicons/react/outline';
+import {SearchIcon, ChevronDownIcon, ChatIcon, UserIcon, BellIcon, LoginIcon, PlusIcon} from '@heroicons/react/outline';
 import Avatar from './avatar.png';
-import HeaderButton from './HeaderButton';
-import {useState} from 'react';
+import {useState, useContext} from 'react';
 import ClickOutHandler from 'react-clickout-handler';
+import AuthModalContext from './AuthModalContext';
+import AccountContext from './AccountContext';
+
 function Header() {
     const [userDropdownVisbility, setUserDropdownVisbility] = useState('hidden')
 
@@ -14,6 +16,11 @@ function Header() {
         setUserDropdownVisbility('hidden');
       }
     }
+
+    const authModal = useContext(AuthModalContext);
+
+    const account = useContext(AccountContext);
+
     return (
         <header className="w-full bg-crypdit_dark p-2">
         <div className="mx-4 flex relative">
@@ -24,28 +31,59 @@ function Header() {
             <input type="text" className="bg-crypdit_dark-search_text text-sm p-1 pl-2 pr-0 block focus:outline-none text-white" placeholder="Searching tldr" />
           </form>
 
+          {account.publicKey && (
+            <>
+              <button className="px-2 py-1">
+                <ChatIcon className="text-gray-400 w-6 h-6 mx-2" />
+              </button>
+              <button className="px-2 py-1">
+                <BellIcon className="text-gray-400 w-6 h-6 mx-2" />
+              </button>
+              <button className="px-2 py-1">
+                <PlusIcon className="text-gray-400 w-6 h-6 mx-2" />
+              </button>
+            </>
+          )}
 
-          <div className='mx-2 hidden sm:block'>
-            <HeaderButton outline className="mr-1">Log In</HeaderButton>
-            <HeaderButton className="">Sign Up</HeaderButton>
-          </div>
+          {!account.publicKey && (
+            <div className='mx-2 hidden sm:block'>
+              <button outline 
+                onClick={() => authModal.setShow(true)} 
+                className="mr-1 h-8 border border-gray-300 rounded-full px-3 text-sm font-bold text-gray-300 hover:bg-gray-300 hover:text-black">
+                Log In
+              </button>
+            </div>
+          )}
           <ClickOutHandler onClickOut={() => setUserDropdownVisbility("hidden")}>
-
             <button className='rounded-md flex ml-4 border border-gray-700' onClick={() => toggleUserDropdown() }>
-              <UserIcon className="w-6 h-6 text-white text-gray-400 m-1"/>
-              {/* <div className="w-8 h-8 bg-gray-600 rounded-md">
-                <img src={Avatar} alt="" style={{filter: 'invert(100%)'}} className="block"/> 
-                
-              </div> */}
+              {!account.publicKey && (
+                <UserIcon className="w-6 h-6 text-white text-gray-400 m-1"/>
+              )}
+              {account.publicKey && (
+                <div className="w-8 h-8 bg-gray-600 rounded-md">
+                  <img src={Avatar} alt="" style={{filter: 'invert(100%)'}} className="block"/> 
+                </div>
+              )}
+              
               <ChevronDownIcon className="text-gray-500 w-5 h-5 mt-2 ml-1" />
             </button>
+            <div className={"absolute right-0 top-8 bg-crypdit_dark border border-gray-700 z-10 rounded-md overflow-hidden text-crypdit_text " + userDropdownVisbility}>
+            {account.publicKey && (
+              <span className="block w-50 py-2 px-3 text-sm">
+                Hello, {account.publicKey.substring(0,6) + "..." + account.publicKey.substring(37, 41)}!
+              </span>
+            )}
+            
+            {!account.publicKey && (
+              <button onClick={() => authModal.setShow(true)} className='block flex w-50 py-2 px-3 hover:bg-gray-300 hover:text-black text-sm'>
+                <LoginIcon className='w-5 h-5 mr-2'/>
+                Log In / Sign Up
+              </button>
+            )}
+            </div>
+
+            
           </ClickOutHandler>
-          <div className={"absolute right-0 top-8 bg-crypdit_dark border border-gray-700 z-10 rounded-md overflow-hidden text-crypdit_text " + userDropdownVisbility}>
-            <button className='block flex w-50 py-2 px-3 hover:bg-gray-300 hover:text-black text-sm'>
-              <LoginIcon className='w-5 h-5 mr-2'/>
-              Log In / Sign up
-            </button>
-          </div>
         </div>
       </header>
     );
